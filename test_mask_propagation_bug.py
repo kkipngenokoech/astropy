52d327be65864a3cc1fc233f6889a2914cab2dda
kipngeno koech <kkipngenokoech22@gmail.com>
2026-04-18 13:15:20 +0200

    fix: handle None masks in NDDataRef mask propagation with bitwise operations

---
+++ test_mask_propagation_bug.py
@@ -0,0 +1,15 @@
+import numpy as np
+from astropy.nddata import NDDataRef
+
+def test_issue_reproduction():
+    # Create two NDDataRef objects, one with a mask and one without
+    data1 = np.array([1, 2, 3])
+    mask1 = np.array([True, False, False])
+    ndd1 = NDDataRef(data1, mask=mask1)
+    
+    data2 = np.array([4, 5, 6])
+    ndd2 = NDDataRef(data2)  # No mask (mask=None)
+    
+    # This should fail with TypeError: unsupported operand type(s) for |: 'int' and 'NoneType'
+    # when handle_mask=np.bitwise_or is used and one operand has no mask
+    result = ndd1.add(ndd2, handle_mask=np.bitwise_or)
