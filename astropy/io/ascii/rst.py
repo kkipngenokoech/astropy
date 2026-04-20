3fc2dfd43466e60f3ff7144499177d3afb57ab7a
kipngeno koech <kkipngenokoech22@gmail.com>
2026-04-20 04:04:37 +0200

    feat: support header_rows parameter in RST format

--- astropy/io/ascii/rst.py
+++ astropy/io/ascii/rst.py
@@ -57,8 +57,10 @@ class RST(FixedWidth):
     data_class = SimpleRSTData
     header_class = SimpleRSTHeader
 
-    def __init__(self):
-        super().__init__(delimiter_pad=None, bookend=False)
+    def __init__(self, **kwargs):
+        # Remove header_rows if present since RST format determines headers from structure
+        kwargs.pop('header_rows', None)
+        super().__init__(delimiter_pad=None, bookend=False, **kwargs)
 
     def write(self, lines):
         lines = super().write(lines)
